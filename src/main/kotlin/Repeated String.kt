e7b15a593203bde73bd23927bcb7fde8a7d19dee
Rudiyanto <rynto14@gmail.com>
2023-06-15 23:18:18 +0700

    repeated string

---
+++ src/main/kotlin/Repeated String.kt
@@ -0,0 +1,20 @@
+// https://www.hackerrank.com/challenges/repeated-string/problem
+
+fun repeatedString(s: String, n: Long): Long {
+    val len = s.length
+    val sumA = MutableList(len) { 0 }
+    var totalA = 0
+    for (i in 0 until len) {
+        if (s[i] == 'a') {
+            totalA += 1
+        }
+        sumA[i] = totalA
+    }
+
+    val left = (n % len).toInt().takeIf {it > 0}?.let { sumA[it - 1] } ?: 0
+    return ((n / len) * totalA) + left
+}
+
+fun main(args: Array<String>) {
+    println(repeatedString("abba", 8))
+}
